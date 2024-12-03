f7b0c2feff3334fb12c4ef5b59dd6e39a99b877d
h2026160 <h2026160@thisdl.cn>
2024-12-03 09:22:20 +0800

    Initial Commit

---
+++ GameTest tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GameTest tvOS
+//
+//  Created by H2026160 on 3/12/2024.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
